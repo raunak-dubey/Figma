a9cef45301ff2c9ab411a4fcdf7f5081e77edeab
Raunak Dubey <raunak18@zohomail.in>
2026-01-21 16:01:23 +0530

    Element Selection Completed.

--- script.js
+++ script.js
@@ -8,23 +8,24 @@ const state = {
 let idCounter = "";
 
 // * generate unique ID
-function generateId() {
+const generateId = () => {
     idCounter = crypto.randomUUID();
     return idCounter;
-}
+};
 
-function createElement(type) {
+const clamp = (value, min, max) => {
+    return Math.max(min, Math.min(value, max));
+};
+
+const createElement = (type) => {
     const id = generateId();
-
+    
     const width = type === "text" ? 120 : 100;
     const height = type === "text" ? 40 : 100;
     let x = 50;
     let y = 50;
 
     const canvasRect = canvas.getBoundingClientRect();
-    function clamp(value, min, max) {
-        return Math.max(min, Math.min(value, max));
-    }
 
     x = clamp(x, 0, canvasRect.width - width);
     y = clamp(y, 0, canvasRect.height - height);
@@ -46,7 +47,7 @@ function createElement(type) {
     renderElement(elementData);
 }
 
-function renderElement(data) {
+const renderElement = (data) => {
     const elem = document.createElement("div");
 
     elem.classList.add("element");
@@ -63,15 +64,44 @@ function renderElement(data) {
 
     if (data.type === "text") {
         elem.textContent = data.text;
-        elem.style.display = "flex";
-        elem.style.alignItems = "center";
-        elem.style.justifyContent = "center";
+        elem.classList.add("text");
         elem.style.fontSize = "16px";
         elem.style.color = "#000";
     }
 
+    elem.addEventListener("click", (e) => {
+        e.stopPropagation();
+        selectElement(data.id);
+    });
+
     canvas.appendChild(elem);
 }
+
+function selectElement(id) {
+    // Deselect previous
+    if (state.selectedId) {
+        const prevSelected = canvas.querySelector(`.element[data-id='${state.selectedId}']`);
+
+        if (prevSelected) prevSelected.classList.remove("selected");
+    }
+
+    // Select new
+    const currentSelected = canvas.querySelector(`.element[data-id='${id}']`);
+    if (currentSelected) {
+        currentSelected.classList.add("selected");
+        state.selectedId = id;
+    } else {
+        state.selectedId = null;
+    }
+}
+
+const deselectElement = (e) => {
+    if (!state.selectedId) return;
+
+    const current = document.querySelector(`.element[data-id='${state.selectedId}']`);
+    if (current) current.classList.remove("selected");
+    state.selectedId = null;
+};
 
 
 document.body.addEventListener('click', (e) => {
@@ -81,8 +111,8 @@ document.body.addEventListener('click', (e) => {
     if (toolName === "shape") {
         createElement("rect");
     }
-    if (toolName === "text") {
+    else if (toolName === "text") {
         createElement("text");
     }
-
 })
+canvas.addEventListener('click', deselectElement);
